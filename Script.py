e79c53e7c807028212ca91b6bf57e4bbeaf206f1
TysonSanju <100722078+TysonSanju@users.noreply.github.com>
2026-09-06 21:03:40 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -269,7 +269,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamxbotz">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/dreamxbotz">[ ᴅʀᴇᴀᴍxʙᴏᴛᴢ ]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/Tyson_Movie_Files">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href="https://t.me/Tyson_Movie_Files">[ Tyson_Movie_Files ]</a></b>"""
 
     
     MOVIE_UPDATE_NOTIFY_TXT = """
